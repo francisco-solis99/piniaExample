4aaf9bec8698525b865f0e827a7370c1c4405f31
francisco-solis99 <franciscosoliscss@gmail.com>
2022-05-02 12:10:40 -0500

    [Add] the pinia state, actions and getters and use it in HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,43 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+<script>
+  import { useCounterStore} from '@/stores/counter.js';
+  import { mapState, mapActions } from "pinia";
+
+  export default {
+    name: 'HomeView',
+    computed: {
+      ...mapState(useCounterStore, ['counter', 'isEvenOrOdd']),
+    },
+    methods: {
+      ...mapActions(useCounterStore, ['increment', 'decrement']),
+    },
+  };
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Home</h1>
+    <p class="count">{{counter}}</p>
+    <div class="buttons">
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+    </div>
+    <p>The number is {{isEvenOrOdd}}</p>
   </main>
 </template>
+
+
+
+<style scoped>
+  .count {
+    font-size: 2.5rem;
+    font-weight: bold;
+  }
+
+  button {
+    font-size: 1.5rem;
+    padding: 0.5rem 1rem;
+    margin: 0.5rem;
+    cursor: pointer;
+  }
+
+</style>
